9fafb912f6559e60e3d0d4bf37e8cc192b33369a
ArtisanRao <digitalproductsartisan@gmail.com>
2025-07-25 11:47:15 +0200

    Revert next.config.mjs to default

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true
 }
 
 export default nextConfig
-export default {
-  async rewrites() {
-    return [
-      {
-        source: '/help',
-        destination: '/help-center/index.html',
-      },
-      {
-        source: '/best-sellers',
-        destination: '/best-sellers/index.html',
-      }
-    ]
-  },
-}
