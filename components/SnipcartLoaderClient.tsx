b11fef13ab83c7727658b6d1af9f9a4730f68bfd
Dadaba Rao <your@email.com>
2025-08-18 00:29:21 +0200

    fix: update Logo component to use logo-new.png and fix SnipcartLoader

--- components/SnipcartLoaderClient.tsx
+++ components/SnipcartLoaderClient.tsx
@@ -1,28 +1,34 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+
+const Snipcart = dynamic(
+  () => import('snipcart'), // if you have a Snipcart React wrapper; else skip
+  { ssr: false }
+);
 
 export default function SnipcartLoaderClient() {
   useEffect(() => {
-    // Inject Snipcart script
-    const script = document.createElement("script");
-    script.src = "https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    // Cleanup on unmount
-    return () => {
-      document.body.removeChild(script);
+    // Configure Snipcart settings
+    window.SnipcartSettings = {
+      locale: 'en-US',
+      currency: 'USD',
     };
   }, []);
 
-  // Snipcart container
   return (
-    <div
-      hidden
-      id="snipcart"
-      data-api-key="ZDgyODMyODgtMzdhZC00ZTI0LTkzZTUtYjRhMTM0MDg4ODM2NjM4ODg4NTc5NTI0NTk5MjQ4"
-      data-config-modal-style="side"
-    ></div>
+    <>
+      <script
+        async
+        src="https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.js"
+      ></script>
+      <div
+        hidden
+        id="snipcart"
+        data-api-key="ZDgyODMyODgtMzdhZC00ZTI0LTkzZTUtYjRhMTM0MDg4ODM2NjM4ODg4NTc5NTI0NTk5MjQ4"
+        data-config-modal-style="side"
+      ></div>
+    </>
   );
 }
