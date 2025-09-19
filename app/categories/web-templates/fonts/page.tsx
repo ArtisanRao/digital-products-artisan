1c545def22822237089ff6ff1f959a21a039c9b7
Dadaba Rao <your@email.com>
2025-09-19 02:46:35 +0200

    feat(categories): wire web-templates/fonts page to shared template

---
+++ app/categories/web-templates/fonts/page.tsx
@@ -0,0 +1,6 @@
+﻿import { CategoryPage, generateCategoryMetadata } from "../../_category";
+
+export const metadata = generateCategoryMetadata("web-templates/fonts");
+export default function Page() {
+  return <CategoryPage slug="web-templates/fonts" />;
+}
