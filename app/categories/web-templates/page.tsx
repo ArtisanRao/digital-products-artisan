00c13c966b1ce2fc0b02385cf6db22a25c0793f9
Dadaba Rao <your@email.com>
2025-09-19 03:06:01 +0200

    fix(categories): normalize wrapper to default+named import for web-templates

--- app/categories/web-templates/page.tsx
+++ app/categories/web-templates/page.tsx
@@ -1,4 +1,4 @@
-﻿import { CategoryPage, generateCategoryMetadata } from "../_category";
+﻿import CategoryPage, { generateCategoryMetadata } from "../_category";
 
 export const metadata = generateCategoryMetadata("web-templates");
 export default function Page() {
