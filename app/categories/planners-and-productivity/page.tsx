cfcbea89025be3d02b4b602a8eb209001181ac34
Dadaba Rao <your@email.com>
2025-09-19 03:05:43 +0200

    fix(categories): normalize wrapper to default+named import for planners-and-productivity

--- app/categories/planners-and-productivity/page.tsx
+++ app/categories/planners-and-productivity/page.tsx
@@ -1,4 +1,4 @@
-﻿import { CategoryPage, generateCategoryMetadata } from "../_category";
+﻿import CategoryPage, { generateCategoryMetadata } from "../_category";
 
 export const metadata = generateCategoryMetadata("planners-and-productivity");
 export default function Page() {
