ad5b90bf5f8882330d6ddce4cd8285e4edd25238
Dadaba Rao <your@email.com>
2025-09-19 03:06:06 +0200

    fix(categories): normalize wrapper to default+named import for web-templates/icons

--- app/categories/web-templates/icons/page.tsx
+++ app/categories/web-templates/icons/page.tsx
@@ -1,4 +1,4 @@
-﻿import { CategoryPage, generateCategoryMetadata } from "../../_category";
+﻿import CategoryPage, { generateCategoryMetadata } from "../../_category";
 
 export const metadata = generateCategoryMetadata("web-templates/icons");
 export default function Page() {
