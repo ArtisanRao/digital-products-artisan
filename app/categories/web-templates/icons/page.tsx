b8579056e0d874c6c0cd74493a96e7756e42bc0a
Dadaba Rao <your@email.com>
2025-09-19 02:46:38 +0200

    feat(categories): wire web-templates/icons page to shared template

---
+++ app/categories/web-templates/icons/page.tsx
@@ -0,0 +1,6 @@
+﻿import { CategoryPage, generateCategoryMetadata } from "../../_category";
+
+export const metadata = generateCategoryMetadata("web-templates/icons");
+export default function Page() {
+  return <CategoryPage slug="web-templates/icons" />;
+}
